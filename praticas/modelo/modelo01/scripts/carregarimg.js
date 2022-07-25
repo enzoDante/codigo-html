a83bfa3b9bb7bbed93b2629fdac8354a14b67614
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2022-07-25 17:37:56 -0300

    update

---
+++ praticas/modelo/modelo01/scripts/carregarimg.js
@@ -0,0 +1,8 @@
+function carregar1(event){
+    let img = document.getElementById("perfil1")
+    img.src = URL.createObjectURL(event.target.files[0])
+}
+function carregar2(event){
+    let img = document.getElementById("perfil2")
+    img.src = URL.createObjectURL(event.target.files[0])
+}
